93412bc7e4a20557235a6ad6c852d6a05fa05cf1
hyejun <127915444+nojungsin@users.noreply.github.com>
2023-06-10 22:58:28 +0900

    Update javaarraylist.java

--- javaarraylist.java
+++ javaarraylist.java
@@ -1,9 +1,9 @@
- Main {
-       public static void main(String[] args) {
-       ArrayList a = new ArrayList();
-       a.add(1);
-       a.add(2);
-       a.add("Hello");
+import java.lang.reflect.Array;
+
+public class Main {
+       Integer[] a = {1,2,3,4};
        System. out.println(a );
+       System. out.println(a [1]);
       }
 }
+[출처] [자바 프로그래밍] 코드 예시 모음 (자바 배우시는 분들 참고하세요.)|작성자 Ryu
