7ea9b01eafb74ef711586db99b70948bf0df8616
hyejun <127915444+nojungsin@users.noreply.github.com>
2023-06-10 23:01:40 +0900

    Update javaarraylist.java

--- javaarraylist.java
+++ javaarraylist.java
@@ -1,10 +1,18 @@
-public class Division {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileRead {
     public static void main(String[] args) {
-        int result = divide(10, 0);
-        System.out.println("Result: " + result);
-    }
-    
-    public static int divide(int num1, int num2) {
-        return num1 / num2;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("nonexistent.txt"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
