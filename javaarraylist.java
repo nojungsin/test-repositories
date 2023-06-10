b234af2c9c135939b773594926cddb7b76324d90
hyejun <127915444+nojungsin@users.noreply.github.com>
2023-06-10 23:06:32 +0900

    Update javaarraylist.java

--- javaarraylist.java
+++ javaarraylist.java
@@ -1,6 +1,8 @@
-public class FailureExample {
+public class InvalidCodeExample {
     public static void main(String[] args) {
-        // 코드 실행 중에 예외 발생
-        throw new RuntimeException("Intentional failure");
+        int x = 5;
+        int y = ;
+        int result = x + y;
+        System.out.println("Result: " + result);
     }
 }
