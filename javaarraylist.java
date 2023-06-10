908d1e2551b92d0b2a58d9218fd01a5fd83d8091
hyejun <127915444+nojungsin@users.noreply.github.com>
2023-06-10 23:07:49 +0900

    Update error  javaarraylist.java

--- javaarraylist.java
+++ javaarraylist.java
@@ -1,8 +1,6 @@
 public class InvalidCodeExample {
     public static void main(String[] args) {
-        int x = 5;
-        int y = ;
-        int result = x + y;
-        System.out.println("Result: " + result);
+        // 문법 오류가 있는 코드
+        System.out.println("Hello, World!"
     }
 }
