6f76604ed65f3e0f7bdbc5a520639e69b7b5b774
hyejun <127915444+nojungsin@users.noreply.github.com>
2023-06-10 23:00:08 +0900

    Update javaarraylist.java

--- javaarraylist.java
+++ javaarraylist.java
@@ -1,9 +1,10 @@
-import java.lang.reflect.Array;
-
-public class Main {
-       Integer[] a = {1,2,3,4};
-       System. out.println(a );
-       System. out.println(a [1]);
-      }
+public class Division {
+    public static void main(String[] args) {
+        int result = divide(10, 0);
+        System.out.println("Result: " + result);
+    }
+    
+    public static int divide(int num1, int num2) {
+        return num1 / num2;
+    }
 }
-[출처] [자바 프로그래밍] 코드 예시 모음 (자바 배우시는 분들 참고하세요.)|작성자 Ryu
